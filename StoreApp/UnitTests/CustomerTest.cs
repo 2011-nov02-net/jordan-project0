9281233bb16c575f38928bb7f14e2c7354a9fbe3
JordanEGarcia <jordaneddygarcia@gmail.com>
2020-11-17 18:08:29 -0600

    starting to implement UnitTests

---
+++ StoreApp/UnitTests/CustomerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using StoreApp.Library;
+using Xunit;
+
+namespace UnitTests
+{
+    public class CustomerTest
+    {
+        [Fact]
+        public void ValidCustomer()
+        {
+            Customer NoName = new Customer(2);
+            Assert.False(NoName.isValid());
+        }
+
+    }
+}
